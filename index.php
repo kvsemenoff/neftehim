e061e4f548755d4ba153e7b487084aa8d57e9dbd
boka <web_masters_07@mail.ru>
2017-05-20 17:19:18 +0300

    struktura

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php require_once('includes/doctype.php'); ?>
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Главная</title>
 <?php require_once('includes/header.php'); ?>
 <div class="n-wrapper">
