85224b03b870379e1f8df3c4e689947c90286be6
Berzeg07 <berezg86@gmail.com>
2017-05-22 00:17:05 +0400

    29.14

---
+++ block/doc-item/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	// $(".doc").click(function(){
+	// 	alert(1);	
+	// });
+
+
+	$(".doc").fancybox(
+	{						
+		"padding" : 10,
+		"imageScale" : false, 
+		"zoomOpacity" : false,
+		"zoomSpeedIn" : 1000,	
+		"zoomSpeedOut" : 1000,	
+		"zoomSpeedChange" : 1000, 
+		"frameWidth" : 700,	 
+		"frameHeight" : 600, 
+		"overlayShow" : true, 
+		"overlayOpacity" : 0.8,	
+		"hideOnContentClick" :false,
+		"centerOnScroll" : false
+	});
+
+});
