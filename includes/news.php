b0b6aebf0eb00bb28d69419766c9b60596fc1291
engine77 <zaur.1kager@gmail.com>
2017-05-22 10:43:08 +0400

    all file

---
+++ includes/news.php
@@ -0,0 +1,30 @@
+<div class="container">
+	<div class="news">
+		<div class="news-item">
+			<a href="#" class="news-item__caption">НАЗВАНИЕ НОВОСТИ</a>
+			<div class="news-item__img">
+				<img src="img/news-item2.jpg" alt="">
+			</div>
+			<a href="#" class="news-item__txt">Nullam eget pharetra est, a pulvinar urna. Sed rutrum commodo dolor vel ullamcorper. Donec placerat ipsum non lacus congue gravida. Quisque lobortis tellus nec felis varius</a>
+			<span class="news-item__date">19.04.2107</span>
+		</div>
+		<div class="news-item">
+			<a href="#" class="news-item__caption">ОЧЕНЬ ДЛИННОЕ НАЗВАНИЕ НОВОСТИ В ДВЕ ДЛИННЫХ, ДЛИННЫХ  СТРОЧКИ</a>
+			<div class="news-item__img">
+				<img src="img/news-item3.jpg" alt="">
+			</div>
+			<a href="#" class="news-item__txt">Nullam eget pharetra est, a pulvinar urna. Sed rutrum commodo dolor vel ullamcorper. Donec placerat ipsum non lacus congue gravida. Quisque lobortis tellus nec felis varius
+				Nullam eget pharetra est, a pulvinar urna. Sed rutrum commodo dolor vel ullamcorper. Donec placerat ipsum non lacus congue </a>
+				<span class="news-item__date">19.04.2107</span>
+			</div>
+			<div class="news-item">
+				<a href="#" class="news-item__caption">НАЗВАНИЕ НОВОСТИ В ОДНУ СТРОЧКУ</a>
+				<div class="news-item__img">
+					<img src="img/news-item1.jpg" alt="">
+				</div>
+				<a href="#" class="news-item__txt">Vivamus dictum erat urna, id tincidunt turpis tempor nec. Phasellus ultrices sapien eget accumsan vehicula. Praesent metus mauris, placerat a odio vitae, fermentum sagittis diam. Vivamus vitae gravida ante. Duis et luctus nunc. Proin volutpat </a>
+				<span class="news-item__date">19.04.2107</span>
+			</div>	
+		</div>
+	</div>
+
