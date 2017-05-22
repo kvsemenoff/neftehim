052c33a703e2dabe193ae9a08b22bd1b062ea482
engine77 <zaur.1kager@gmail.com>
2017-05-22 19:10:51 +0400

    all file

--- includes/product.php
+++ includes/product.php
@@ -5,19 +5,19 @@
 				<div class="product-section__img">
 					<a href="#"><img src="img/product1.jpg" alt=""></a>
 				</div>
-				<a href="#" class="button-more"><span>ПАРАФИНО-ВОСКОВАЯ ПРОДУКЦИЯ </span></a>
+				<a href="#" class="button-more button-more_width"><span>ПАРАФИНО-ВОСКОВАЯ ПРОДУКЦИЯ </span></a>
 			</div>
 			<div class="product-section__box">
 				<div class="product-section__img">
 					<a href="#"><img src="img/product2.jpg" alt=""></a>
 				</div>
-				<a href="#" class="button-more"><span>БИТУМЫ И МАЗУТЫ</span></a>
+				<a href="#" class="button-more button-more_width"><span>БИТУМЫ И МАЗУТЫ</span></a>
 			</div>
 			<div class="product-section__box product-section__box_last">
 				<div class="product-section__img">
 					<a href="#"><img src="img/product3.jpg" alt=""></a>
 				</div>
-				<a href="#" class="button-more"><span>НЕФТЕХИМИЯ</span></a>
+				<a href="#" class="button-more button-more_width"><span>НЕФТЕХИМИЯ</span></a>
 			</div>
 		</div>
 	
