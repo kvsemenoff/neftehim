357c66be3497b38863872b4ad2b0d38790d12bab
Berzeg07 <berezg86@gmail.com>
2017-05-22 18:11:08 +0400

    29.14

--- includes/header-top.php
+++ includes/header-top.php
@@ -8,7 +8,7 @@
 		<div class="col-xs-12 col-sm-4 col-md-4">
 			<div class="header-box__right">
 				<span>8 499 <b>685 10 26</b></span><br/>
-				<button class="button">Оставьте сообщение</button>
+				<a href="js-form1" class="button js-modal">Оставьте сообщение</a>
 			</div>
 		</div>
 		<div class="clearfix"></div>
