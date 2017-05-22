2cb5b692171a75f4339c02c4aea017b0810b5fe5
Berzeg07 <berezg86@gmail.com>
2017-05-22 13:46:09 +0400

    29.14

--- catalog.php
+++ catalog.php
@@ -3,20 +3,68 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Каталог</title>
-<?php require_once('includes/header.php'); ?>
-<div class="n-wrapper">
-	<div class="n-header">
-		<?php require_once('includes/header-top.php'); ?>
-	</div>
-	<div class="n-content">
-		<div class="n-content-inner vertical-align">	
+	<title>Каталог</title>
+	<?php require_once('includes/header.php'); ?>
+	<div class="n-wrapper">
+		<div class="n-header">
+			<?php require_once('includes/header-top.php'); ?>
+		</div>
+		<div class="n-content">
+			<div class="n-content-inner vertical-align">	
 
+				<div class="container">
+					<div class="box">
+						<div class="product-item">
+							<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>
+							<div class="product-item__txtbox">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+							</div>
+							<div class="product-item__buttons">
+								<span class="product-item__pasport">Паспорта и сертификаты</span>
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
+						</div>
+
+						<div class="product-item">
+							<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>
+							<div class="product-item__txtbox">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+							</div>
+							<div class="product-item__buttons">
+								<span class="product-item__pasport">Паспорта и сертификаты</span>
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
+						</div>
+
+						<div class="product-item">
+							<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>
+							<div class="product-item__txtbox">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+							</div>
+							<div class="product-item__buttons">
+								<span class="product-item__pasport">Паспорта и сертификаты</span>
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="n-footer">
+			<?php require_once('includes/footer.php'); ?>
 		</div>
 	</div>
-	<div class="n-footer">
-		<?php require_once('includes/footer.php'); ?>
-	</div>
-</div>
 </body>
 </html>
