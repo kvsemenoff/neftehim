06ccf6ef0c24dd25d79f75d21c7f8c21a40c0274
engine77 <zaur.1kager@gmail.com>
2017-05-24 10:26:37 +0400

    all file

--- bitums.php
+++ bitums.php
@@ -15,132 +15,176 @@
 			<div class="n-content-inner vertical-align bitumsbgimg">	
 				<div class="container newsbg bitumsblock">				
 					<h1 class="h2 about__caption">БИТУМЫ И МАЗУТЫ</h1>
+
+
 					<div class="product-item">
-						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
-						<div class="product-item__img">
-							<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
-						</div>
-						<div class="product-item__txtbox">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>	
+						<div class="product-item__txtbox">	
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>		
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.							
+							</p>	
 						</div>
 						<div class="product-item__buttons">
 							<span class="product-item__pasport">Паспорта и сертификаты</span>
-							<a href="#" class="product-item__look">Посмотреть</a>
-							<a href="#" class="product-item__download">Скачать</a>
+							<div class="product-item__boxbuttons">
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
 						</div>
+						<div class="clearfix"></div>
 					</div>
 					<div class="product-item">
-						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
-						<div class="product-item__img">
-							<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
-						</div>
-						<div class="product-item__txtbox">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>	
+						<div class="product-item__txtbox">	
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>		
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.	
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.	
+							</p>	
 						</div>
 						<div class="product-item__buttons">
 							<span class="product-item__pasport">Паспорта и сертификаты</span>
-							<a href="#" class="product-item__look">Посмотреть</a>
-							<a href="#" class="product-item__download">Скачать</a>
+							<div class="product-item__boxbuttons">
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
 						</div>
+						<div class="clearfix"></div>
 					</div>
 					<div class="product-item product-item_last">
-						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
-						<div class="product-item__img">
-							<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
-						</div>
-						<div class="product-item__txtbox">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>	
+						<div class="product-item__txtbox">	
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>		
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.						
+							</p>	
 						</div>
 						<div class="product-item__buttons">
 							<span class="product-item__pasport">Паспорта и сертификаты</span>
-							<a href="#" class="product-item__look">Посмотреть</a>
-							<a href="#" class="product-item__download">Скачать</a>
+							<div class="product-item__boxbuttons">
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
 						</div>
+						<div class="clearfix"></div>
 					</div>
 					<div class="product-item">
-						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
-						<div class="product-item__img">
-							<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
-						</div>
-						<div class="product-item__txtbox">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>	
+						<div class="product-item__txtbox">	
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>		
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.							
+							</p>	
 						</div>
 						<div class="product-item__buttons">
 							<span class="product-item__pasport">Паспорта и сертификаты</span>
-							<a href="#" class="product-item__look">Посмотреть</a>
-							<a href="#" class="product-item__download">Скачать</a>
+							<div class="product-item__boxbuttons">
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
 						</div>
+						<div class="clearfix"></div>
 					</div>
 					<div class="product-item">
-						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
-						<div class="product-item__img">
-							<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
-						</div>
-						<div class="product-item__txtbox">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>	
+						<div class="product-item__txtbox">	
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>		
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.						
+							</p>	
 						</div>
 						<div class="product-item__buttons">
 							<span class="product-item__pasport">Паспорта и сертификаты</span>
-							<a href="#" class="product-item__look">Посмотреть</a>
-							<a href="#" class="product-item__download">Скачать</a>
+							<div class="product-item__boxbuttons">
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
 						</div>
+						<div class="clearfix"></div>
 					</div>
 					<div class="product-item product-item_last">
-						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
-						<div class="product-item__img">
-							<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
-						</div>
-						<div class="product-item__txtbox">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>	
+						<div class="product-item__txtbox">	
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>		
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.						
+							</p>	
 						</div>
 						<div class="product-item__buttons">
 							<span class="product-item__pasport">Паспорта и сертификаты</span>
-							<a href="#" class="product-item__look">Посмотреть</a>
-							<a href="#" class="product-item__download">Скачать</a>
+							<div class="product-item__boxbuttons">
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
 						</div>
+						<div class="clearfix"></div>
 					</div>
 					<div class="product-item">
-						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
-						<div class="product-item__img">
-							<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
-						</div>
-						<div class="product-item__txtbox">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>	
+						<div class="product-item__txtbox">	
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>		
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.							
+							</p>	
 						</div>
 						<div class="product-item__buttons">
 							<span class="product-item__pasport">Паспорта и сертификаты</span>
-							<a href="#" class="product-item__look">Посмотреть</a>
-							<a href="#" class="product-item__download">Скачать</a>
+							<div class="product-item__boxbuttons">
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
 						</div>
+						<div class="clearfix"></div>
 					</div>
 					<div class="product-item">
-						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
-						<div class="product-item__img">
-							<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
-						</div>
-						<div class="product-item__txtbox">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>	
+						<div class="product-item__txtbox">	
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>		
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.						
+							</p>	
 						</div>
 						<div class="product-item__buttons">
 							<span class="product-item__pasport">Паспорта и сертификаты</span>
-							<a href="#" class="product-item__look">Посмотреть</a>
-							<a href="#" class="product-item__download">Скачать</a>
+							<div class="product-item__boxbuttons">
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
 						</div>
+						<div class="clearfix"></div>
 					</div>
 					<div class="product-item product-item_last">
-						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>
-						<div class="product-item__img">
-							<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
-						</div>
-						<div class="product-item__txtbox">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.
+						<h4 class="product-item__caption">НАИМЕНОВАНИЕ ПРОДУКТА</h4>	
+						<div class="product-item__txtbox">	
+							<div class="product-item__img">
+								<a href="../../img/product-item1.jpg" data-fancybox-group="group"><img src="../../img/product-item1.jpg" alt=""></a>
+							</div>		
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris mattis ante id ipsum vulputate, et ultrices purus bibendum. Vivamus in nulla  maximus.						
+							</p>	
 						</div>
 						<div class="product-item__buttons">
 							<span class="product-item__pasport">Паспорта и сертификаты</span>
-							<a href="#" class="product-item__look">Посмотреть</a>
-							<a href="#" class="product-item__download">Скачать</a>
+							<div class="product-item__boxbuttons">
+								<a href="#" class="product-item__look">Посмотреть</a>
+								<a href="#" class="product-item__download">Скачать</a>
+							</div>
 						</div>
+						<div class="clearfix"></div>
 					</div>
+
+					
+
+
+
 
 					<div class="news-button"><a href="#" class="button-more button-more_width"><span>ПЕРЕЙТИ К ПОЛНОМУ КАТАЛОГУ ПРОДУКЦИИ</span></a></div>
 				</div>
