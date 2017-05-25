56b11c5231e4bd9714d716a45ea0ac904a37d610
engine77 <zaur.1kager@gmail.com>
2017-05-25 14:33:10 +0400

    all file

--- news.php
+++ news.php
@@ -50,12 +50,12 @@
 						<span class="news-item__date">19.04.2107</span>
 					</div>					
 					<div class="news-item">
-						<a href="#" class="news-item__caption">ОЧЕНЬ ДЛИННОЕ НАЗВАНИЕ НОВОСТИ В ДВЕ ДЛИННЫХ, ДЛИННЫХ  СТРОЧКИ</a>
+						<a href="#" class="news-item__caption">НАЗВАНИЕ НОВОСТИ</a>
 						<div class="news-item__img">
 							<a href="#"><img src="img/news-item3.jpg" alt=""></a>
 						</div>
 						<a href="#" class="news-item__txt">Nullam eget pharetra est, a pulvinar urna. Sed rutrum commodo dolor vel ullamcorper. Donec placerat ipsum non lacus congue gravida. Quisque lobortis tellus nec felis varius
-							Nullam eget pharetra est, a pulvinar urna. Sed rutrum commodo dolor vel ullamcorper. Donec placerat ipsum non lacus congue </a>
+							 </a>
 							<span class="news-item__date">19.04.2107</span>
 					</div>					
 					<div class="news-item">
@@ -63,7 +63,7 @@
 						<div class="news-item__img">
 							<a href="#"><img src="img/news-item1.jpg" alt=""></a>
 						</div>
-						<a href="#" class="news-item__txt">Vivamus dictum erat urna, id tincidunt turpis tempor nec. Phasellus ultrices sapien eget accumsan vehicula. Praesent metus mauris, placerat a odio vitae, fermentum sagittis diam. Vivamus vitae gravida ante. Duis et luctus nunc. Proin volutpat </a>
+						<a href="#" class="news-item__txt">Vivamus dictum erat urna, id tincidunt turpis tempor nec. Phasellus ultrices sapien eget accumsan vehicula. Praesent metus mauris, placerat a odio vitae, fermentum sagittis diam. </a>
 						<span class="news-item__date">19.04.2107</span>
 					</div>
 					<div class="news-item">
@@ -75,7 +75,7 @@
 						<span class="news-item__date">19.04.2107</span>
 					</div>					
 					<div class="news-item">
-						<a href="#" class="news-item__caption">ОЧЕНЬ ДЛИННОЕ НАЗВАНИЕ НОВОСТИ В ДВЕ ДЛИННЫХ, ДЛИННЫХ  СТРОЧКИ</a>
+						<a href="#" class="news-item__caption">НАЗВАНИЕ НОВОСТИ</a>
 						<div class="news-item__img">
 							<a href="#"><img src="img/news-item3.jpg" alt=""></a>
 						</div>
@@ -91,7 +91,7 @@
 						<a href="#" class="news-item__txt">Vivamus dictum erat urna, id tincidunt turpis tempor nec. Phasellus ultrices sapien eget accumsan vehicula. Praesent metus mauris, placerat a odio vitae, fermentum sagittis diam. Vivamus vitae gravida ante. Duis et luctus nunc. Proin volutpat </a>
 						<span class="news-item__date">19.04.2107</span>
 					</div>			
-
+					<div class="clearfix"></div>
 					<div class="news-button">
 						<a href="#" class="button-more button-more_width">
 							<span class="button-more__bg">
