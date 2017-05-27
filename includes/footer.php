bdafc03ef201140234ab838385f9bf6a03ba1d4e
kvsemenoff <kvsemenoff@mail.ru>
2017-05-27 14:39:37 +0400

    f

--- includes/footer.php
+++ includes/footer.php
@@ -46,29 +46,25 @@
 
 <a href="#" class="gototop"></a>
 
-
-	<!-- JS_BLOCK -->
-	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
-	<script src="libs/owl.carousel/owl.carousel.js"></script>
-	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
-	<!-- <script type="text/javascript" src="js/jquery.mousewheel.js"></script> -->
-	<!-- <script src="js/responsiveTabs.js"></script> -->
-	<script src="js/jquery.maskedinput.min.js"></script>
-	<script src="js/fonts.js"></script>
-	<script src="js/common.js"></script>
-	<script src="https://api-maps.yandex.ru/2.1/?lang=tr_TR" type="text/javascript"></script>
-	<script src="js/map.js"></script>
-	<script src="block/a/script.js"></script>
-	<script src="block/box/script.js"></script>
-	<script src="block/button/script.js"></script>
-	<script src="block/button-more/script.js"></script>
-	<script src="block/doc-item/script.js"></script>
-	<script src="block/h1/script.js"></script>
-	<script src="block/h2/script.js"></script>
-	<script src="block/input/script.js"></script>
-	<script src="block/news-item/script.js"></script>
-	<script src="block/product-item/script.js"></script>
-	<script src="block/search-box/script.js"></script>
-	<script src="block/strong/script.js"></script>
-	<script src="block/submit/script.js"></script>
-	<script src="block/textarea/script.js"></script>
+<script src="https://api-maps.yandex.ru/2.1/?lang=tr_TR" type="text/javascript"></script>
+<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+<script src="libs/owl.carousel/owl.carousel.js"></script>
+<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.maskedinput.min.js"></script>
+<script src="js/fonts.js"></script>
+<script src="js/common.js"></script>
+<script src="js/map.js"></script>
+<script src="block/a/script.js"></script>
+<script src="block/box/script.js"></script>
+<script src="block/button/script.js"></script>
+<script src="block/button-more/script.js"></script>
+<script src="block/doc-item/script.js"></script>
+<script src="block/h1/script.js"></script>
+<script src="block/h2/script.js"></script>
+<script src="block/input/script.js"></script>
+<script src="block/news-item/script.js"></script>
+<script src="block/product-item/script.js"></script>
+<script src="block/search-box/script.js"></script>
+<script src="block/strong/script.js"></script>
+<script src="block/submit/script.js"></script>
+<script src="block/textarea/script.js"></script>
