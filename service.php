2ed481e528dbb91c2b18c498f2f3e9964c547ae8
engine77 <zaur.1kager@gmail.com>
2017-05-29 17:11:34 +0400

    all file

--- service.php
+++ service.php
@@ -43,7 +43,7 @@
 									<div class="doc-wrap">
 										<div class="doc-item">
 											<a class="doc" data-fancybox-group=group	href="../../img/doc.jpg"><img src="../../img/doc.jpg" alt=""></a><br/>
-											<a href="#">Скачать</a>
+											<a href="#" class="docs__download docs__download-hidden">Скачать</a>
 										</div>
 									</div>
 									<div class="doc-wrap">
@@ -72,7 +72,7 @@
 									<div class="doc-wrap">
 										<div class="doc-item">
 											<a class="doc" data-fancybox-group=group	href="../../img/doc.jpg"><img src="../../img/doc.jpg" alt=""></a><br/>
-											<a href="#">Скачать</a>
+											<a href="#" class="docs__download">Скачать</a>
 										</div>
 									</div>
 								</div>
